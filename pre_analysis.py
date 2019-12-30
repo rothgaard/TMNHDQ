888d4e720408adcfd0e3930fac2b500a3817365f
rothgaard <alex.fernandes.lseg@gmail.com>
2019-12-30 17:14:44 +0000

    added story points derived stats

--- pre_analysis.py
+++ pre_analysis.py
@@ -4,6 +4,31 @@ import matplotlib.pyplot as plt
 
 from datetime import datetime
 
+
+def epic_estimated_vs_actual(team_size, avg_wip, avg_days_story):
+    conv_factor_eff_days = float(5/7)*0.8
+
+    data_epic_jira = pd.read_csv("./data/jira_epic_data.csv", index_col=0)
+    data_epic_processed = pd.read_csv("./data/processed_data.csv", index_col=0)
+
+    for index, row in data_epic_processed.iterrows():
+        item = row
+        jira_data = data_epic_jira.loc[data_epic_jira['doc_key'] == item['doc_key']]
+        if jira_data.empty:
+            continue
+        elif int(jira_data['story_points'].values[0]) == 0:
+            continue
+
+        actual_by_sp = int(jira_data['story_points'].values[0])*(team_size/avg_wip)*avg_days_story*conv_factor_eff_days
+        estimate_by_md = item['estimate']
+        print("\nEpic: %s " % row['doc_key'])
+        print("Original Estimate: %s" % estimate_by_md)
+        print("Actual: %0.0f" % actual_by_sp)
+        print("Error (Man/Days): %0.0f" %  (actual_by_sp-estimate_by_md))
+        error_pct = 100*(actual_by_sp/estimate_by_md - 1.0)
+        print("Error (%%): %0.0f" %  error_pct)
+
+
 def main():
     data_stories = pd.read_csv("./data/jira_story_data.csv", index_col=0)
     print("Stats for Cycle Time") 
@@ -37,6 +62,8 @@ def main():
     print("Average Allocation of Team Members / Story : %0.2f" % tm_story)
     print("Average  Days / Story Point: %0.2f" % avg_days_sp)
     #print("Average Allocation of Team Members / Story Point : %0.2f" % tm_sp)
+
+    epic_estimated_vs_actual(team_size, awip, avg_days_sp)
 
 
 
